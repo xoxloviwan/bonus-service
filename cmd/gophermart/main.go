00038293a7bff656491740488fe7a580bcfe9897
iwankho <43066471+xoxloviwan@users.noreply.github.com>
2024-09-02 20:54:32 +0300

    call server.Shutdown

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"gophermart/internal/api"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 const port = ":8080"
 
 func main() {
 	router := api.Router()
+	server := &http.Server{
+		Addr:    port,
+		Handler: router,
+	}
 	errCh := make(chan error)
 
 	go func() {
-		errCh <- http.ListenAndServe(port, router)
+		errCh <- server.ListenAndServe()
 	}()
 	api.Log.Info(fmt.Sprintf("service listening on %s", port))
 
@@ -26,10 +32,18 @@ func main() {
 	for {
 		select {
 		case <-sigCh:
-			api.Log.Info("service stopped by SIGINT")
+			api.Log.Info("calling SIGINT")
+			ctx, cancelCtx := context.WithTimeout(context.TODO(), 5*time.Second)
+			defer cancelCtx()
+			err := server.Shutdown(ctx)
+			if err != nil {
+				api.Log.Error(fmt.Sprintf("shutdown error: %s", err))
+				os.Exit(1)
+			}
+			api.Log.Info("service gracefully stopped")
 			return
 		case err := <-errCh:
-			api.Log.Error(fmt.Sprintf("service stopped with error:\n%s", err))
+			api.Log.Error(fmt.Sprintf("service stopped with error: %s", err))
 			os.Exit(1)
 		}
 	}
